ee719be683d33a94e4e9bea53dc1720147b5dd06
Roman <roman.battalov224@gmail.com>
2025-03-26 22:56:20 +0500

    Лаба 1

---
+++ LanguagePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Батталов_школа
+{
+    /// <summary>
+    /// Логика взаимодействия для LanguagePage.xaml
+    /// </summary>
+    public partial class LanguagePage : Page
+    {
+        public LanguagePage()
+        {
+            InitializeComponent();
+            LanguageListView.ItemsSource = Батталов_LanguageEntities.GetContext().Client.ToList();
+
+        }
+    }
+}
